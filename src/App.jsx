684a490169bb12c88128c3ee98ae111ac998243a
DivyanshuSahu2000 <sdivyanshusahu172@gmail.com>
2026-01-17 23:50:42 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,50 @@
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increaseByNum, increment } from "./redux/slices/countSlice";
 import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  const dispatch = useDispatch();
+  const count = useSelector((state) => state.counter.value);
+  const [num, setNum] = useState(8);
+  return (
+    <div className="container">
+      <h1></h1>
+      {count}
+      <div className="main ">
+        <button
+          onClick={() => {
+            dispatch(increment());
+          }}
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => {
+            dispatch(decrement());
+          }}
+        >
+          Decrement
+        </button>
+      </div>
+      <div className="inputsec">
+        <button
+          onClick={() => {
+            dispatch(increaseByNum(Number(num)));
+          }}
+        >
+          increase by {Number(num)}
+        </button>
+        <input
+          type="number"
+          value={num}
+          onChange={(e) => {
+            setNum(e.target.value);
+            console.log(e.target.value);
+          }}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/redux/slices/countSlice.js
+++ src/redux/slices/countSlice.js
@@ -1,3 +1,22 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const countSlice = createSlice();
+export const counterSlice = createSlice({
+  name: "counter",
+  initialState: {
+    value: 0,
+  },
+  reducers: {
+    increment: (state) => {
+      state.value += 1;
+    },
+    decrement: (state) => {
+      state.value -= 1;
+    },
+    increaseByNum: (state, actions) => {
+      state.value += actions.payload;
+    },
+  },
+});
+export const { increment, decrement, increaseByNum } = counterSlice.actions;
+const counterReducer = counterSlice.reducer;
+export default counterReducer;
